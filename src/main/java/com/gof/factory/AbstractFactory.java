1d97202f5a4151a35725cbd359de8f94edba7ae9
walker_yin <walker_yin@EB36291550>
2018-10-18 10:57:07 +0800

    init

--- src/main/java/com/gof/factory/AbstractFactory.java
+++ src/main/java/com/gof/factory/AbstractFactory.java
@@ -4,7 +4,7 @@ package com.gof.factory;
  * Created by shinho on 2018/10/18.
  */
 public class AbstractFactory {
-    public static void mian(String[] args){
+    public static void main(String[] args){
 
     }
 }

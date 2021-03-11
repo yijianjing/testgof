ecefac529402930eeeb2e7afdaff9a80543c79b7
殷建军 <yinjianjun@shinhofood.com>
2021-03-11 16:17:19 +0800

    模板模式

---
+++ src/main/java/com/gof/template/jdbc/MemberDaoTest.java
@@ -0,0 +1,24 @@
+package com.gof.template.jdbc;
+
+
+import com.gof.template.jdbc.dao.MemberDao;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by Tom.
+ */
+public class MemberDaoTest {
+
+    public static void main(String[] args) {
+        MemberDao memberDao = new MemberDao(null);
+        List<?> result = memberDao.selectAll();
+        System.out.println(result);
+    }
+}
